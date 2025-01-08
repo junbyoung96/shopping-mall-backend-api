39f027de4611d330819f23bedb01c38acde68a7c
junbyoung96 <junbyoung96@gmail.com>
2025-01-08 16:20:42 +0900

    add database configuration

--- src/main/java/com/allra/shop_backend/ShopBackendApplication.java
+++ src/main/java/com/allra/shop_backend/ShopBackendApplication.java
@@ -2,8 +2,10 @@ package com.allra.shop_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ShopBackendApplication {
 
 	public static void main(String[] args) {
